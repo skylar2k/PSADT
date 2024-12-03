efe8d61ecc082a295464abf5b30669ad2b8bbe86
Skylar Johansen <skylar@unix.rs>
2024-12-03 11:30:29 +0100

    Remove Zero-Config msi from template

--- TEMPLATE/Deploy-Application.ps1
+++ TEMPLATE/Deploy-Application.ps1
@@ -1,85 +1,4 @@
-﻿<#
-.SYNOPSIS
-
-PSApppDeployToolkit - This script performs the installation or uninstallation of an application(s).
-
-.DESCRIPTION
-
-- The script is provided as a template to perform an install or uninstall of an application(s).
-- The script either performs an "Install" deployment type or an "Uninstall" deployment type.
-- The install deployment type is broken down into 3 main sections/phases: Pre-Install, Install, and Post-Install.
-
-The script dot-sources the AppDeployToolkitMain.ps1 script which contains the logic and functions required to install or uninstall an application.
-
-PSApppDeployToolkit is licensed under the GNU LGPLv3 License - (C) 2024 PSAppDeployToolkit Team (Sean Lillis, Dan Cunningham and Muhammad Mashwani).
-
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the
-Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-for more details. You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-.PARAMETER DeploymentType
-
-The type of deployment to perform. Default is: Install.
-
-.PARAMETER DeployMode
-
-Specifies whether the installation should be run in Interactive, Silent, or NonInteractive mode. Default is: Interactive. Options: Interactive = Shows dialogs, Silent = No dialogs, NonInteractive = Very silent, i.e. no blocking apps. NonInteractive mode is automatically set if it is detected that the process is not user interactive.
-
-.PARAMETER AllowRebootPassThru
-
-Allows the 3010 return code (requires restart) to be passed back to the parent process (e.g. SCCM) if detected from an installation. If 3010 is passed back to SCCM, a reboot prompt will be triggered.
-
-.PARAMETER TerminalServerMode
-
-Changes to "user install mode" and back to "user execute mode" for installing/uninstalling applications for Remote Desktop Session Hosts/Citrix servers.
-
-.PARAMETER DisableLogging
-
-Disables logging to file for the script. Default is: $false.
-
-.EXAMPLE
-
-powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeployMode 'Silent'; Exit $LastExitCode }"
-
-.EXAMPLE
-
-powershell.exe -Command "& { & '.\Deploy-Application.ps1' -AllowRebootPassThru; Exit $LastExitCode }"
-
-.EXAMPLE
-
-powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeploymentType 'Uninstall'; Exit $LastExitCode }"
-
-.EXAMPLE
-
-Deploy-Application.exe -DeploymentType "Install" -DeployMode "Silent"
-
-.INPUTS
-
-None
-
-You cannot pipe objects to this script.
-
-.OUTPUTS
-
-None
-
-This script does not generate any output.
-
-.NOTES
-
-Toolkit Exit Code Ranges:
-- 60000 - 68999: Reserved for built-in exit codes in Deploy-Application.ps1, Deploy-Application.exe, and AppDeployToolkitMain.ps1
-- 69000 - 69999: Recommended for user customized exit codes in Deploy-Application.ps1
-- 70000 - 79999: Recommended for user customized exit codes in AppDeployToolkitExtensions.ps1
-
-.LINK
-
-https://psappdeploytoolkit.com
-#>
-
-
-[CmdletBinding()]
+﻿[CmdletBinding()]
 Param (
     [Parameter(Mandatory = $false)]
     [ValidateSet('Install', 'Uninstall', 'Repair')]
@@ -184,35 +103,18 @@ Try {
         [String]$installPhase = 'Pre-Installation'
         ## <Perform Pre-Installation tasks here>
 
-
         ##*===============================================
         ##* MARK: INSTALLATION
         ##*===============================================
         [String]$installPhase = 'Installation'
-
-        ## Handle Zero-Config MSI Installations
-        If ($useDefaultMsi) {
-            [Hashtable]$ExecuteDefaultMSISplat = @{ Action = 'Install'; Path = $defaultMsiFile }; If ($defaultMstFile) {
-                $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile)
-            }
-            Execute-MSI @ExecuteDefaultMSISplat; If ($defaultMspFiles) {
-                $defaultMspFiles | ForEach-Object { Execute-MSI -Action 'Patch' -Path $_ }
-            }
-        }
         ## <Perform Installation tasks here>
-
 
         ##*===============================================
         ##* MARK: POST-INSTALLATION
         ##*===============================================
         [String]$installPhase = 'Post-Installation'
         ## <Perform Post-Installation tasks here>
-        
-        
-        ## Display a message at the end of the install
-        If (-not $useDefaultMsi) {
-            #Show-InstallationPrompt -Message 'You can customize text to appear at the end of an install or remove it completely for unattended installations.' -ButtonRightText 'OK' -Icon Information -NoWait
-        }
+
     }
     ElseIf ($deploymentType -ieq 'Uninstall') {
         ##*===============================================
@@ -221,19 +123,10 @@ Try {
         [String]$installPhase = 'Pre-Uninstallation'
         ## <Perform Pre-Uninstallation tasks here>
 
-
         ##*===============================================
         ##* MARK: UNINSTALLATION
         ##*===============================================
         [String]$installPhase = 'Uninstallation'
-
-        ## Handle Zero-Config MSI Uninstallations
-        If ($useDefaultMsi) {
-            [Hashtable]$ExecuteDefaultMSISplat = @{ Action = 'Uninstall'; Path = $defaultMsiFile }; If ($defaultMstFile) {
-                $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile)
-            }
-            Execute-MSI @ExecuteDefaultMSISplat
-        }
         ## <Perform Uninstallation tasks here>
 
         ##*===============================================
@@ -254,14 +147,6 @@ Try {
         ##* MARK: REPAIR
         ##*===============================================
         [String]$installPhase = 'Repair'
-
-        ## Handle Zero-Config MSI Repairs
-        If ($useDefaultMsi) {
-            [Hashtable]$ExecuteDefaultMSISplat = @{ Action = 'Repair'; Path = $defaultMsiFile; }; If ($defaultMstFile) {
-                $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile)
-            }
-            Execute-MSI @ExecuteDefaultMSISplat
-        }
         ## <Perform Repair tasks here>
 
         ##*===============================================
