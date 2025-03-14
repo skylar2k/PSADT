1bbe8c6f5808aa255ceaffee00fdb603643ebd45
Skylar Johansen <skylar@unix.rs>
2025-03-14 10:23:24 +0100

    ArcGIS Desktop

---
+++ ArcGIS Desktop/10.8.2/Invoke-AppDeployToolkit.ps1
@@ -0,0 +1,173 @@
+﻿[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('Install', 'Uninstall', 'Repair')]
+    [PSDefaultValue(Help = 'Install', Value = 'Install')]
+    [System.String]$DeploymentType,
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('Interactive', 'Silent', 'NonInteractive')]
+    [PSDefaultValue(Help = 'Silent', Value = 'Silent')]
+    [System.String]$DeployMode,
+
+    [Parameter(Mandatory = $false)]
+    [System.Management.Automation.SwitchParameter]$AllowRebootPassThru,
+
+    [Parameter(Mandatory = $false)]
+    [System.Management.Automation.SwitchParameter]$TerminalServerMode,
+
+    [Parameter(Mandatory = $false)]
+    [System.Management.Automation.SwitchParameter]$DisableLogging
+)
+
+
+##================================================
+## MARK: Variables
+##================================================
+
+$adtSession = @{
+    # App variables.
+    AppVendor                   = 'Esri'
+    AppName                     = 'ArcGIS Desktop'
+    AppVersion                  = '10.8.2'
+    AppArch                     = 'x86_64'
+    AppLang                     = 'EN'
+    AppRevision                 = '01'
+    AppSuccessExitCodes         = @(0)
+    AppRebootExitCodes          = @(1641, 3010)
+    AppScriptVersion            = '1.0.0'
+    AppScriptDate               = '2025-03-11'
+    AppScriptAuthor             = 'Skylar Johansen'
+
+    # Install Titles (Only set here to override defaults set by the toolkit).
+    InstallName                 = ''
+    InstallTitle                = ''
+
+    # Script variables.
+    DeployAppScriptFriendlyName = $MyInvocation.MyCommand.Name
+    DeployAppScriptVersion      = '4.0.6'
+    DeployAppScriptParameters   = $PSBoundParameters
+}
+
+$global:secrets = $null
+
+function Install-ADTDeployment {
+    ##================================================
+    ## MARK: Pre-Install
+    ##================================================
+    $adtSession.InstallPhase = "Pre-$($adtSession.DeploymentType)"
+
+    ##================================================
+    ## MARK: Install
+    ##================================================
+    $adtSession.InstallPhase = $adtSession.DeploymentType
+    Start-ADTMsiProcess -Action 'Install' -FilePath 'Setup.msi' -ArgumentList @(
+        '/QN'
+        'ACCEPTEULA=Yes'
+        'ENABLEEUEI=0' # Disable Esri User Experience Improvement program
+    )
+
+    ##================================================
+    ## MARK: Post-Install
+    ##================================================
+    $adtSession.InstallPhase = "Post-$($adtSession.DeploymentType)"
+    Set-ADTRegistryKey -Key "HKLM\Software\MRFK\$($adtSession.AppVendor)\$($adtSession.AppName)" -Name 'Version' -Type 'String' -Value $adtSession.AppVersion
+
+}
+
+function Uninstall-ADTDeployment {
+    ##================================================
+    ## MARK: Pre-Uninstall
+    ##================================================
+    $adtSession.InstallPhase = "Pre-$($adtSession.DeploymentType)"
+
+    ##================================================
+    ## MARK: Uninstall
+    ##================================================
+    $adtSession.InstallPhase = $adtSession.DeploymentType
+    Uninstall-ADTApplication -FilterScript { $_.UninstallSubKey -match "$($adtSession.AppName)" } -AdditionalArgumentList '/qn'
+
+    ##================================================
+    ## MARK: Post-Uninstallation
+    ##================================================
+    $adtSession.InstallPhase = "Post-$($adtSession.DeploymentType)"
+    Remove-ADTRegistryKey -Key "HKLM\Software\MRFK\$($adtSession.AppVendor)\$($adtSession.AppName)" -Recurse
+}
+
+function Repair-ADTDeployment {
+    ##================================================
+    ## MARK: Pre-Repair
+    ##================================================
+    $adtSession.InstallPhase = "Pre-$($adtSession.DeploymentType)"
+
+    ##================================================
+    ## MARK: Repair
+    ##================================================
+    $adtSession.InstallPhase = $adtSession.DeploymentType
+
+    ##================================================
+    ## MARK: Post-Repair
+    ##================================================
+    $adtSession.InstallPhase = "Post-$($adtSession.DeploymentType)"
+
+}
+
+
+##================================================
+## MARK: Initialization
+##================================================
+
+# Set strict error handling across entire operation.
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+Set-StrictMode -Version 1
+
+# Import the module and instantiate a new session.
+try {
+    $moduleName = if ([System.IO.File]::Exists("$PSScriptRoot\PSAppDeployToolkit\PSAppDeployToolkit.psd1")) {
+        Get-ChildItem -LiteralPath $PSScriptRoot\PSAppDeployToolkit -Recurse -File | Unblock-File -ErrorAction Ignore
+        "$PSScriptRoot\PSAppDeployToolkit\PSAppDeployToolkit.psd1"
+    }
+    else {
+        'PSAppDeployToolkit'
+    }
+    Import-Module -FullyQualifiedName @{ ModuleName = $moduleName; Guid = '8c3c366b-8606-4576-9f2d-4051144f7ca2'; ModuleVersion = '4.0.6' } -Force
+    try {
+        $iadtParams = Get-ADTBoundParametersAndDefaultValues -Invocation $MyInvocation
+        $adtSession = Open-ADTSession -SessionState $ExecutionContext.SessionState @adtSession @iadtParams -PassThru
+        $secrets = Get-Content "$($adtSession.dirSupportFiles)\secrets.json" | ConvertFrom-Json
+    }
+    catch {
+        Remove-Module -Name PSAppDeployToolkit* -Force
+        throw
+    }
+}
+catch {
+    $Host.UI.WriteErrorLine((Out-String -InputObject $_ -Width ([System.Int32]::MaxValue)))
+    exit 60008
+}
+
+
+##================================================
+## MARK: Invocation
+##================================================
+
+try {
+    Get-Item -Path $PSScriptRoot\PSAppDeployToolkit.* | & {
+        process {
+            Get-ChildItem -LiteralPath $_.FullName -Recurse -File | Unblock-File -ErrorAction Ignore
+            Import-Module -Name $_.FullName -Force
+        }
+    }
+    & "$($adtSession.DeploymentType)-ADTDeployment"
+    Close-ADTSession
+}
+catch {
+    Write-ADTLogEntry -Message ($mainErrorMessage = Resolve-ADTErrorRecord -ErrorRecord $_) -Severity 3
+    Show-ADTDialogBox -Text $mainErrorMessage -Icon Stop | Out-Null
+    Close-ADTSession -ExitCode 60001
+}
+finally {
+    Remove-Module -Name PSAppDeployToolkit* -Force
+}
